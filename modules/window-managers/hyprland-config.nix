29ae3838027bfb0d4ba3bb386946862a8dd8992f
Yiyuan He <yiyuanh@nixos.me>
2024-07-01 20:38:43 -0700

    feat: add basic config to hyprland

--- modules/window-managers/hyprland-config.nix
+++ modules/window-managers/hyprland-config.nix
@@ -9,11 +9,36 @@
 
   config = lib.mkIf config.hyprland-config.enable {
 
-    wayland.windowManager.hyprland = {
+    wayland.windowManager.hyprland.enable = true;
 
-      enable = true;
-
-    };    
+    wayland.windowManager.hyprland.settings = {
+      "$mod" = "SUPER";
+      bind =
+        [
+          "$mod, F, exec, firefox"
+          "$mod, Return, exec, alacritty"
+          "$mod SHIFT, Q, exec, hyprctl dispatch exit"
+          "$mod, W, killactive"
+          ", Print, exec, grimblast copy area"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+          );
+        monitor = ",preferred,auto,1";
+      };
 
   };
 
